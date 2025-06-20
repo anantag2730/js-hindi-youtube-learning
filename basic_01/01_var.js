d8fb61573a1fa6b8c3af99f87892d1b6d3e55009
anantag2730 <anantag2730@gmail.com>
2025-06-20 14:10:08 +0000

    container config done

---
+++ basic_01/01_var.js
@@ -0,0 +1,20 @@
+const accountID=15536
+let accountName="Anant Agarwal"
+var accountPassword="anant@27"
+accountCity="Jaipur"
+let accountState;
+console.log(accountID);
+console.log(accountName);
+console.log(accountPassword);
+console.log(accountCity);
+// accountID=2 Not allowed
+console.log(accountID);
+
+accountName="HItendra Agarwal"
+accountPassword="an@3027"
+accountCity="Bengluru"
+
+console.log(accountName);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountID,accountName,accountPassword,accountCity,accountState]);
---
+++ basic_01/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"// treat all JS code as newer version
+//alert(3+3) // we are using node js not browser 
+
+let name="anant"// string data type
+let age=3// number data type
+let loggedIn=false// boolean data type
+
+// number ->range 2^53
+//bigint 
+// string=""
+// boolean= true or false
+// null->stand alone value
+// undefined ->value not assigned yet 
+// symbol()-> to tell uniqueness mainly used in react 
+console.table([typeof "Anant",typeof 2,typeof null,typeof undefined])
