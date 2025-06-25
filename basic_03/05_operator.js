bcd9a93476ad822de57d6e231a3e5b10688e6bc7
anantag2730 <anantag2730@gmail.com>
2025-06-25 17:14:17 +0000

    conatiner config done

---
+++ basic_03/05_operator.js
@@ -0,0 +1,16 @@
+// node basic_03/05_operator.js
+
+// NULL Coalesing Operator(??): null undefined
+
+let val1;
+val1=5 ?? 10
+console.log(val1);
+
+val1="anant"??50
+console.log(val1);
+
+val1=null ?? 10
+console.log(val1);
+
+val1=undefined ?? 100
+console.log(val1);
